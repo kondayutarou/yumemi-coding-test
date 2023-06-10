09499ed5000e86975cd1fcdcf74ba48c6161d0ac
Yutaro Konda <kondayutarou@gmail.com>
2023-06-10 17:54:55 +0900

    Redux用にActionを一通り作る

---
+++ iOSEngineerCodeCheck/Action/GithubRepositoryAction.swift
@@ -0,0 +1,17 @@
+//
+//  GithubRepositoryAction.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Yutaro on 2023/06/10.
+//  Copyright © 2023 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
+
+enum GithubRepositoryAction {
+    case fetchGithubRepositoryList(query: String)
+    case didReceiveGithubRepositoryList(result: [GithubRepositoryListItemResponse])
+    case didReceiveError
+    case fetchAvatarImage(avatarURL: String)
+    case didReceiveAvatarImage(data: Data)
+}
