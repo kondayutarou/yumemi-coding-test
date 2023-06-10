0ce92b1ca6881b709890ae925213d758b7d03f61
Yutaro Konda <kondayutarou@gmail.com>
2023-06-10 10:38:07 +0900

    unused_optional_binding

--- iOSEngineerCodeCheck/SceneDelegate.swift
+++ iOSEngineerCodeCheck/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         // swiftlint:enable line_length
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard scene as? UIWindowScene != nil else { return }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
