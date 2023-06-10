3546b072d06b275e45ff28a2794158acc8c0e60d
Yutaro Konda <kondayutarou@gmail.com>
2023-06-10 18:27:19 +0900

    Redux用にreducer作る

---
+++ iOSEngineerCodeCheck/Store/Reducer/AppReducer.swift
@@ -0,0 +1,20 @@
+//
+//  AppReducer.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Yutaro on 2023/06/10.
+//  Copyright © 2023 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
+
+func appReducer(_ state: AppState, _ action: Action) -> AppState {
+    var state = state
+    
+    switch action {
+    case .githubRepository(let action):
+        state.githubRepositoryState = githubRepositoryReducer(state.githubRepositoryState, action: action)
+    }
+    
+    return state
+}
---
+++ iOSEngineerCodeCheck/Store/Reducer/GithubRepositoryReducer.swift
@@ -0,0 +1,22 @@
+//
+//  GithubRepositoryReducer.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Yutaro on 2023/06/10.
+//  Copyright © 2023 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
+
+func githubRepositoryReducer(_ state: GithubRepositoryState, action: GithubRepositoryAction) -> GithubRepositoryState {
+    var state = state
+    
+    switch action {
+    case let .didReceiveGithubRepositoryList(result: result):
+        state.repositoryList = result
+    default:
+        break
+    }
+    
+    return state
+}
