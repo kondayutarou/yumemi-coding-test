246725cffd0796067d39eed9f3c6f9ac4625a8c8
Yutaro Konda <kondayutarou@gmail.com>
2023-06-11 19:54:08 +0900

    SwiftLintはSPMで入れる。またbuild前のrun scriptにSwiftLint追加

--- iOSEngineerCodeCheck/View/Details/DetailsViewController.swift
+++ iOSEngineerCodeCheck/View/Details/DetailsViewController.swift
@@ -22,7 +22,8 @@ final class DetailsViewController: UIViewController {
     var cancellableSet: Set<AnyCancellable> = []
 
     static func make(viewData: GithubRepositoryListItemResponse) -> DetailsViewController? {
-        guard let viewController = R.storyboard.detailsViewController.instantiateInitialViewController() else { return nil }
+        guard let viewController = R.storyboard.detailsViewController.instantiateInitialViewController()
+        else { return nil }
 
         viewController.viewData = viewData
         return viewController
